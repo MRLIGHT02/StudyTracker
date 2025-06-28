3a9f23928596403d6ff8eb0ea8376f0917d79337
MRLIGHT02 <saifali42020@gmail.com>
2025-06-29 00:20:58 +0530

    Enhance custom entry control and update XAML layout
    Updated `CustomEnteryBorder.cs` to add methods for handling background color changes and included a new using directive. Modified `AddSessionPage.xaml` to incorporate a new XML namespace for custom controls and streamlined the layout by replacing existing entry elements with the enhanced custom entry control.

--- StudyTracker/CustomControls/CustomEnteryBorder.cs
+++ StudyTracker/CustomControls/CustomEnteryBorder.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Maui.Handlers;
@@ -14,11 +15,34 @@ namespace StudyTracker.CustomControls
         protected override void OnHandlerChanged()
         {
             base.OnHandlerChanged();
+            SetBorderBackGround();
+        }
+        protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            base.OnPropertyChanged(propertyName);
+            if (propertyName == nameof(BackgroundColor))
+            {
+                SetBorderBackGround();
+            }
+        }
+        private void SetBorderBackGround()
+        {
+
 #if ANDROID
             if (Handler is IEntryHandler entryHandler)
+
             {
-                entryHandler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Colors.Transparent.ToPlatform());
+                if (BackgroundColor == null)
+                {
+                    entryHandler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Colors.Transparent.ToPlatform());
+                }
+                else
+                {
+
+                    entryHandler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(BackgroundColor.ToPlatform());
+                }
             }
+
 #endif
         }
     }
