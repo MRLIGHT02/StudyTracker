52d1cec7a2403a57453118a834e81b7775cb1dab
MRLIGHT02 <saifali42020@gmail.com>
2025-07-01 16:52:54 +0530

    #

--- StudyTracker/CustomControls/CustomEnteryBorder.cs
+++ StudyTracker/CustomControls/CustomEnteryBorder.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Maui.Controls.Platform;
 using Microsoft.Maui.Handlers;
 using Microsoft.Maui.Platform;
 
@@ -16,12 +17,14 @@ namespace StudyTracker.CustomControls
         {
             base.OnHandlerChanged();
             SetBorderBackGround();
+            FontSize = 20;
         }
         protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             base.OnPropertyChanged(propertyName);
             if (propertyName == nameof(BackgroundColor))
             {
+
                 SetBorderBackGround();
             }
         }
@@ -32,15 +35,28 @@ namespace StudyTracker.CustomControls
             if (Handler is IEntryHandler entryHandler)
 
             {
+                var shape = new Android.Graphics.Drawables.GradientDrawable();
+                shape.SetShape(Android.Graphics.Drawables.ShapeType.Rectangle);
                 if (BackgroundColor == null)
                 {
                     entryHandler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Colors.Transparent.ToPlatform());
+
                 }
                 else
                 {
 
                     entryHandler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(BackgroundColor.ToPlatform());
+                    shape.SetStroke(2, Android.Graphics.Color.Black);
+                    shape.SetCornerRadius(10f);
+                    entryHandler.PlatformView.SetBackground(shape);
                 }
+
+                int leftpadding = 0;
+                int rightpadding = 0;
+                int toppadding = 0;
+                int bottompadding = 20;
+                entryHandler.PlatformView.SetPadding(leftpadding, toppadding, rightpadding, bottompadding);
+
             }
 
 #endif
